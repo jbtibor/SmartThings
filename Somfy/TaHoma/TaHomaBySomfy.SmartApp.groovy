7fd0fed00e2bdfbeda440cd7328571bd1e7d81d0
jbtibor <valaszoljnekem@gmail.com>
2018-07-03 21:31:13 +0100

    Support illuminance sensor. Refactorings.

--- Somfy/TaHoma/TaHomaBySomfy.SmartApp.groovy
+++ Somfy/TaHoma/TaHomaBySomfy.SmartApp.groovy
@@ -11,6 +11,11 @@
  *  See the License for the specific language governing permissions and limitations under the License.
  *
  */
+
+ /**
+  * TODO:
+  * Store TaHoma commands in map and reference map from Device Types so that Device Types can be generic.
+  */
 definition(
 		name: "TaHoma® by Somfy",
 		namespace: "jbt",
@@ -45,6 +50,7 @@ def settingsPage() {
 	updateDevices()
 
 	def interiorRollerBlindNames = getInteriorRollerBlindNames()
+	def lightSensorNames = getLightIOSystemSensorNames()
 	def rollerShutterNames = getRollerShutterNames()
 	def rollerShutterWithLowSpeedManagementIOComponentNames = getRollerShutterWithLowSpeedManagementIOComponentNames()
 
@@ -53,6 +59,7 @@ def settingsPage() {
 	dynamicPage(name: "settingsPage", title: "", install: true, uninstall: true) {
 		section ("TaHoma® devices to control", hideWhenEmpty: true) {
 			input(name: "selectedInteriorRollerBlindNames", type: "enum", title: "Interior Roller Blinds", description: "Tap to choose", required: false, multiple: true, metadata: [values: interiorRollerBlindNames], displayDuringSetup: true)
+			input(name: "selectedLightIOSystemSensorNames", type: "enum", title: "Light Sensors", description: "Tap to choose", required: false, multiple: true, metadata: [values: lightSensorNames], displayDuringSetup: true)
 			input(name: "selectedRollerShutterNames", type: "enum", title: "Roller Shutters RTS", description: "Tap to choose", required: false, multiple: true, metadata: [values: rollerShutterNames], displayDuringSetup: true)
 			input(name: "selectedRollerShutterWithLowSpeedManagementIOComponentNames", type: "enum", title: "Roller Shutters IO", description: "Tap to choose", required: false, multiple: true, metadata: [values: rollerShutterWithLowSpeedManagementIOComponentNames], displayDuringSetup: true)
 			input(name: "selectedSwitchNames", type: "enum", title: "Switches", description: "Tap to choose", required: false, multiple: true, metadata: [values: switchNames], displayDuringSetup: true)
@@ -133,6 +140,7 @@ def initialize() {
 	setDefaultValues();
 
 	def selectedDeviceCount = processSelectedInteriorRollerBlinds()
+	selectedDeviceCount += processSelectedLightIOSystemSensors()
 	selectedDeviceCount += processSelectedRollerShutters()
 	selectedDeviceCount += processSelectedRollerShuttersWithLowSpeedManagementIOComponent()
 	selectedDeviceCount += processSelectedSwitches()
@@ -190,6 +198,58 @@ def processSelectedInteriorRollerBlinds() {
 	debug("User selected ${selectedInteriorRollerBlinds.size()} Interior Roller Blinds.")
 
 	return selectedInteriorRollerBlinds.size()
+}
+
+def processSelectedLightIOSystemSensors() {
+	debug("processSelectedLightIOSystemSensors()")
+
+	if (!settings.selectedLightIOSystemSensorNames){
+		settings.selectedLightIOSystemSensorNames = []
+	}
+
+	log.debug("selectedLightIOSystemSensorNames $selectedLightIOSystemSensorNames")
+	def selectedLightIOSystemSensors = selectedLightIOSystemSensorNames.each { dni ->
+		def lightIOSystemSensor = atomicState.lightIOSystemSensors[dni]
+
+		if (!lightIOSystemSensor) {
+			debug("initialize: lightIOSystemSensors: ${atomicState.lightIOSystemSensors}")
+
+			def errorMessage = "Light IO System Sensor '$dni' not found."
+			log.error(errorMessage)
+
+			throw new org.json.JSONException(errorMessage)
+		}
+
+		debug("lightIOSystemSensor lightIOSystemSensor")
+
+		def deviceName = getDeviceName(lightIOSystemSensor)
+		def deviceId = getDeviceId(lightIOSystemSensor)
+
+		def virtualDevice = getChildDevice(dni)
+
+		if (virtualDevice) {
+			debug("Found ${virtualDevice.name} with network id '$dni' already exists.")
+		} else {
+			def deviceTypeName = "TaHoma Light Sensor IO"
+
+			debug("Creating new '$deviceTypeName' device '$deviceName' with id '$dni'.")
+
+			def capabilities = [:]
+
+			virtualDevice = addChildDevice(app.namespace, deviceTypeName, dni, null, ["name": deviceId, "label": deviceName, "completedSetup": true])
+			virtualDevice.setCapabilities(capabilities)
+
+			debug("virtualDevice ${virtualDevice}")
+
+			debug("Created '$deviceName' with network id '$dni'.")
+		}
+
+		return virtualDevice
+	}
+
+	debug("User selected ${selectedLightIOSystemSensors.size()} TaHoma Light Sensor IO devices.")
+
+	return selectedLightIOSystemSensors.size()
 }
 
 def processSelectedRollerShutters() {
@@ -348,6 +408,7 @@ def deleteUnselectedDevices() {
 	def delete
 
 	if ((settings.selectedInteriorRollerBlindNames && settings.selectedInteriorRollerBlindNames.size() > 0) 
+		|| (settings.selectedLightIOSystemSensorNames && settings.selectedLightIOSystemSensorNames.size() > 0) 
 		|| (settings.selectedRollerShutterNames && settings.selectedRollerShutterNames.size() > 0) 
 		|| (settings.selectedRollerShutterWithLowSpeedManagementIOComponentNames && settings.selectedRollerShutterWithLowSpeedManagementIOComponentNames.size() > 0) 
 		|| (settings.selectedSwitchNames && settings.selectedSwitchNames.size() > 0)) {
@@ -355,6 +416,7 @@ def deleteUnselectedDevices() {
 
 		delete = getChildDevices().findAll { 
 			!settings.selectedInteriorRollerBlindNames.contains(it.deviceNetworkId) &&
+			!settings.selectedLightIOSystemSensorNames.contains(it.deviceNetworkId) &&
             !settings.selectedRollerShutterNames.contains(it.deviceNetworkId) &&
             !settings.selectedRollerShutterWithLowSpeedManagementIOComponentNames.contains(it.deviceNetworkId) &&
             !settings.selectedSwitchNames.contains(it.deviceNetworkId)
@@ -463,6 +525,20 @@ def removeChildDevices(childDevices) {
     }
 }
 
+def getDeviceNames(devices) {
+	debug("getDeviceNames($devices)")
+
+	def names = [:]
+
+	devices.each { dni, device ->
+		names[dni] = getDeviceName(device)
+	}
+
+	debug("names $names")
+
+	return names
+}
+
 def getInteriorRollerBlindNames() {
 	debug("getInteriorRollerBlindNames()")
 
@@ -475,6 +551,16 @@ def getInteriorRollerBlindNames() {
 	debug("interiorRollerBlindNames $interiorRollerBlindNames")
 
 	return interiorRollerBlindNames
+}
+
+def getLightIOSystemSensorNames() {
+	debug("getLightIOSystemSensorNames()")
+
+	def names = getDeviceNames(atomicState.lightIOSystemSensors)
+
+	debug("names $names")
+
+	return names
 }
 
 def getRollerShutterNames() {
@@ -545,20 +631,24 @@ def updateChildDevices() {
 			try {
 				def eventData = [:]
 
-				def deviceData = atomicState.rollerShuttersWithLowSpeedManagementIOComponent[childDevice.deviceNetworkId]
+				def deviceData = atomicState.rawDeviceData[childDevice.deviceNetworkId]
+                
+                log.debug("deviceData.states ${deviceData.states}")
 
 				if (deviceData && deviceData.states) {
 					deviceData.states.each { state ->
                     	//log.debug("state $state")
 						if (state.name == "core:ClosureState") {
 							eventData["level"] = state.value
+						} else if (state.name == "core:LuminanceState") {
+							eventData["illuminance"] = state.value
 						}
 					}
 				}
 
 				log.debug("Event data ${eventData}")
 
-				childDevice.generateEvent(eventData)
+				childDevice.generateEvents(eventData)
 			} catch (Exception e) {
 				log.error("Exception updating device ${childDevice.name}: ${e}")
 			}
@@ -587,6 +677,8 @@ def updateDevices() {
 			debug("updateDevices(): resp.data ${resp.data}")
 
 			if (resp.status == 200 && resp.data) {
+				def lightIOSystemSensors = [:]
+				def rawDeviceData = [:]
 				def rollerShutters = [:]
 				def rollerShuttersWithLowSpeedManagementIOComponent = [:]
 				def interiorRollerBlinds = [:]
@@ -595,6 +687,7 @@ def updateDevices() {
 				//def slurper = new groovy.json.JsonSlurper()
 				//def result = slurper.parseText('{"devices": [{"creationTime": 1524144461000,      "lastUpdateTime": 1524144461000,      "label": "Kidsroom east",      "deviceURL": "rts://1201-2886-4711/16776321",      "shortcut": false,      "controllableName": "io:RollerShutterWithLowSpeedManagementIOComponent",      "states": [        {          "name": "core:NameState",          "type": 3,          "value": "Kidsroom east"        },        {          "name": "core:PriorityLockTimerState",          "type": 1,          "value": 0        },        {          "name": "core:StatusState",          "type": 3,          "value": "available"        },        {          "name": "core:RSSILevelState",          "type": 2,          "value": 80.0        },        {          "name": "core:ClosureState",          "type": 1,          "value": 66        },        {          "name": "core:OpenClosedState",          "type": 3,          "value": "open"        }      ],      "attributes": [],      "available": true,      "enabled": true,      "placeOID": "45bf640c-b9fb-4703-8ec6-4d88e49abd2d",      "widget": "PositionableRollerShutterWithLowSpeedManagement",      "type": 1,      "oid": "56f526d7-cffc-4460-91a8-3bb53b6ed0ca",      "uiClass": "RollerShutter" }]}')
 				//def result = slurper.parseText('{"devices": [{"creationTime": 1524144461000,      "lastUpdateTime": 1524144461000,      "label": "IO test blind",      "deviceURL": "rts://1201-2886-4711/16776321",      "shortcut": false,      "controllableName": "io:RollerShutterGenericIOComponent",      "states": [        {          "name": "core:NameState",          "type": 3,          "value": "IO test blind"        },        {          "name": "core:PriorityLockTimerState",          "type": 1,          "value": 0        },        {          "name": "core:StatusState",          "type": 3,          "value": "available"        },        {          "name": "core:RSSILevelState",          "type": 2,          "value": 80.0        },        {          "name": "core:ClosureState",          "type": 1,          "value": 66        },        {          "name": "core:OpenClosedState",          "type": 3,          "value": "open"        }      ],      "attributes": [],      "available": true,      "enabled": true,      "placeOID": "45bf640c-b9fb-4703-8ec6-4d88e49abd2d",      "widget": "PositionableRollerShutterWithLowSpeedManagement",      "type": 1,      "oid": "56f526d7-cffc-4460-91a8-3bb53b6ed0ca",      "uiClass": "RollerShutter" }]}')
+				//def result = slurper.parseText('{"devices": [{"creationTime": 1524144461000,      "lastUpdateTime": 1524144461000,      "label": "IO test light sensor",      "deviceURL": "io://1201-2886-666/777",      "shortcut": false,      "controllableName": "io:LightIOSystemSensor",      "states": [{ "name": "core:LuminanceState", "type": 2, "value": 90000.0 }      ],      "attributes": [],      "available": true,      "enabled": true,      "placeOID": "45bf640c-b9fb-4703-8ec6-4d88e49abd2d",      "widget": "PositionableRollerShutterWithLowSpeedManagement",      "type": 1,      "oid": "56f526d7-cffc-4460-91a8-3bb53b6ed0ca",      "uiClass": "RollerShutter" }]}')
 				//resp.data.devices = result.devices
 				if (resp.data.devices) {
 					resp.data.devices.each { device ->
@@ -604,24 +697,36 @@ def updateDevices() {
 
 						if (device.controllableName == 'rts:BlindRTSComponent') {
 							interiorRollerBlinds[dni] = device
+							rawDeviceData[dni] = device
 						} else if (device.controllableName == 'rts:RollerShutterRTSComponent') {
 							rollerShutters[dni] = device
+							rawDeviceData[dni] = device
 						} else if (device.controllableName == 'rts:OnOffRTSComponent') {
 							switches[dni] = device
+							rawDeviceData[dni] = device
+						} else if (device.controllableName == 'io:LightIOSystemSensor') {
+							lightIOSystemSensors[dni] = device
+							rawDeviceData[dni] = device
 						} else if (device.controllableName == 'io:RollerShutterWithLowSpeedManagementIOComponent') {
 							rollerShuttersWithLowSpeedManagementIOComponent[dni] = device
+							rawDeviceData[dni] = device
 						} else if (device.controllableName == 'io:RollerShutterGenericIOComponent') {
 							rollerShuttersWithLowSpeedManagementIOComponent[dni] = device
+							rawDeviceData[dni] = device
 						}
 					}
 				}
 
 				debug("updateDevices(): interiorRollerBlinds ${interiorRollerBlinds}")
+				debug("updateDevices(): lightIOSystemSensors ${lightIOSystemSensors}")
+				debug("updateDevices(): rawDeviceData ${rawDeviceData}")
 				debug("updateDevices(): rollerShutters ${rollerShutters}")
 				debug("updateDevices(): rollerShuttersWithLowSpeedManagementIOComponent ${rollerShuttersWithLowSpeedManagementIOComponent}")
 				debug("updateDevices(): switches ${switches}")
 
 				atomicState.interiorRollerBlinds = interiorRollerBlinds
+				atomicState.lightIOSystemSensors = lightIOSystemSensors
+                atomicState.rawDeviceData = rawDeviceData
 				atomicState.rollerShutters = rollerShutters
 				atomicState.rollerShuttersWithLowSpeedManagementIOComponent = rollerShuttersWithLowSpeedManagementIOComponent
 				atomicState.switches = switches
@@ -640,8 +745,8 @@ def updateDevices() {
 	}
 }
 
-def executeRollerShutterCommand(commandId, rollerShutterId, label, parameters = "") {
-	debug("executeRollerShutterCommand($commandId, $rollerShutterId, $label, $parameters)")
+def executeCommand(commandId, rollerShutterId, label, parameters = "") {
+	debug("executeCommand($commandId, $rollerShutterId, $label, $parameters)")
 
 	label = "$label ($rollerShutterId; SmartThings $smartAppVersion)";
 
@@ -664,70 +769,25 @@ def executeRollerShutterCommand(commandId, rollerShutterId, label, parameters = 
 	try {
 		httpPostJson(requestParams) { resp ->
 			if (resp.status == 200 && resp.data) {
-				debug("executeRollerShutterCommand(): resp.data ${resp.data}")
+				debug("executeCommand(): resp.data ${resp.data}")
 
 				refresh()
 
 				executionId = resp.data.execId
 			}
 			else {
-				log.error("executeRollerShutterCommand(): Failed: ${resp.status}")
+				log.error("executeCommand(): Failed: ${resp.status}")
 			}
 		}
 	} catch (groovyx.net.http.HttpResponseException e) {
-		log.error("executeRollerShutterCommand(): Error: ${e}")
+		log.error("executeCommand(): Error: ${e}")
 
 		if (e.statusCode == 401) {
 			atomicState.authorizationFailed = true
 		}
 	}
 
-	debug("executeRollerShutterCommand: $commandId done: executionId: $executionId")
-
-	return executionId
-}
-
-def executeSwitchCommand(commandId, switchId, label) {
-	debug("executeSwitchCommand($commandId, $switchId, $label)")
-
-	label = "$label ($switchId; SmartThings $smartAppVersion)";
-
-	def body = "{\"label\":\"$label\",\"actions\":[{\"deviceURL\":\"$switchId\",\"commands\":[{\"name\":\"$commandId\",\"parameters\":[]}]}]}"
-
-	def requestParams = [
-		method: 'POST',
-		uri: cloudApiEndpoint,
-		path: 'exec/apply',
-		headers: [
-			'Cookie': getAuthorizationHeaderValue()
-		],
-		body: body
-	]
-
-	debug("requestParams: $requestParams")
-
-	def executionId
-
-	try {
-		httpPostJson(requestParams) { resp ->
-			if (resp.status == 200 && resp.data) {
-				debug("executeSwitchCommand(): resp.data ${resp.data}")
-
-				executionId = resp.data.execId
-			}
-			else {
-				log.error("executeSwitchCommand(): Failed: ${resp.status}")
-			}
-		}
-	} catch (groovyx.net.http.HttpResponseException e) {
-		log.error("executeSwitchCommand(): Error: ${e}")
-
-		if (e.statusCode == 401) {
-			atomicState.authorizationFailed = true
-		}
-	}
-
-	debug("executeSwitchCommand: $commandId done: executionId: $executionId")
+	debug("executeCommand: $commandId done: executionId: $executionId")
 
 	return executionId
 }
@@ -768,7 +828,7 @@ def stopExecution(executionId, label) {
 def close(rollerShutterId, label) {
 	debug("close($rollerShutterId, $label)")
 
-	def executionId = executeRollerShutterCommand("close", rollerShutterId, "Close $label")
+	def executionId = executeCommand("close", rollerShutterId, "Close $label")
 
 	return executionId
 }
@@ -776,7 +836,7 @@ def close(rollerShutterId, label) {
 def identify(rollerShutterId, label) {
 	debug("identify($rollerShutterId, $label)")
 
-	def executionId = executeRollerShutterCommand("identify", rollerShutterId, "Identify $label")
+	def executionId = executeCommand("identify", rollerShutterId, "Identify $label")
 
 	return executionId
 }
@@ -784,7 +844,7 @@ def identify(rollerShutterId, label) {
 def open(rollerShutterId, label) {
 	debug("open($rollerShutterId, $label)")
 
-	def executionId = executeRollerShutterCommand("open", rollerShutterId, "Open $label")
+	def executionId = executeCommand("open", rollerShutterId, "Open $label")
 
 	return executionId
 }
@@ -792,7 +852,7 @@ def open(rollerShutterId, label) {
 def presetPosition(rollerShutterId, label) {
 	debug("presetPosition($rollerShutterId, $label)")
 
-	def executionId = executeRollerShutterCommand("my", rollerShutterId, "My position $label")
+	def executionId = executeCommand("my", rollerShutterId, "My position $label")
 
 	return executionId
 }
@@ -806,7 +866,7 @@ def stop(executionId, label) {
 def switchOff(switchId, label) {
 	debug("switchOff($switchId, $label)")
 
-	def executionId = executeSwitchCommand("off", switchId, "$label off")
+	def executionId = executeCommand("off", switchId, "$label off")
 
 	return executionId
 }
@@ -814,7 +874,7 @@ def switchOff(switchId, label) {
 def switchOn(switchId, label) {
 	debug("switchOn($switchId, $label)")
 
-	def executionId = executeSwitchCommand("on", switchId, "$label on")
+	def executionId = executeCommand("on", switchId, "$label on")
 
 	return executionId
 }
