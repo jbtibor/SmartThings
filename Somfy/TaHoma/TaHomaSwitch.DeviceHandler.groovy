0c26698d58f87258497ffba507d436bb810c3fa7
jbtibor <valaszoljnekem@gmail.com>
2018-07-03 21:34:38 +0100

    Refactoring.

--- Somfy/TaHoma/TaHomaSwitch.DeviceHandler.groovy
+++ Somfy/TaHoma/TaHomaSwitch.DeviceHandler.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2017 Tibor Jakab-Barthi
+ *  Copyright (c) 2017-2018 Tibor Jakab-Barthi
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -40,13 +40,17 @@ preferences {
 }
 
 def getDeviceTypeVersion() {
-	"1.1.20171222" 
+	"1.1.20180703" 
 }
 
 def debug(message) {
 	if (parent.settings.debugMode) {
 		log.debug("DT $deviceTypeVersion: $message")
 	}
+}
+
+def generateEvents(Map eventData) {
+	debug("generateEvents(${eventData})")
 }
 
 // Switch
